65278a215d220bf4b0b2f54b56bbb5b78f661bb6
Athena Hernandez <athenahernandezc@gmail.com>
2026-09-01 16:18:12 -0700

    AWS integration

---
+++ content-manager/helloworld.py
@@ -0,0 +1,26 @@
+# Name: Coco Hernandez
+# Date: August 31, 2026
+
+def thoughts():
+    print("I don't remember the last time I wrote in a text editor like this." \
+    "Without AI, I mean." \
+    "Back in 9th grade, while other teenagers my age looked forward to parties and playing drinking games," \
+    "I looked forward to opening VSCode and my pink Terminal, and playing with 0s and 1s." \
+    "Coding used to feel like a puzzle." \
+    "And I don't want to overromanticize that feeling because in retrospect it's easy to." \
+    "Parsing (me, not a machine) through Stack Overflow threads," \
+    "adding random print statements hoping to backtrace my error," \
+    "it was slow," \
+    "but it was rewarding." \
+    "Nowadays, it takes me seconds to figure out what's wrong with my code." \
+    "Or what the next \"best\" step might be." \
+    "People are scared that AI is going to take their jobs, and I am very much too." \
+    "Especially as I realize the only thing it's good for is writing code." \
+    "But at times I rediscover my lost wonder for 0s and 1s" \
+    "when I'm able to dream bigger & reach the clouds." \
+    "I just (and have always since I was a little girl) want to make an impact" \
+    "for better." \
+    "And to do so, my gut tells me to use the skills that /skill can't do." \
+    "That's what makes us human. :)")
+
+thoughts()
